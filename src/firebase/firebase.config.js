ef0db6a5b44cd31733ae00b1660f93e8f4a83a64
biswasbittu <biswasbittu7415@yahoo.com>
2023-05-02 00:30:16 +0530

    project instrument install done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQaSeAB-fZP-cTlGFtX2q-sAvNxy4WIWY",
+  authDomain: "assignment-10-chef.firebaseapp.com",
+  projectId: "assignment-10-chef",
+  storageBucket: "assignment-10-chef.appspot.com",
+  messagingSenderId: "286876243384",
+  appId: "1:286876243384:web:bc51d322d3b0385428ab4d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
