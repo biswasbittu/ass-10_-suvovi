879e8429092fa9024273b60260e57971033931e7
biswasbittu <biswasbittu7415@yahoo.com>
2023-05-02 10:41:31 +0530

    router setup done

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './Home';
+
+const Home = () => {
+    return (
+        <div>
+            This is Home
+        </div>
+    );
+};
+
+export default Home;
