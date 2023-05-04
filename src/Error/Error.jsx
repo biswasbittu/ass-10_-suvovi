d2cf6d3ed1d98f0e94e904fc70a1126654e4de29
biswasbittu <biswasbittu7415@yahoo.com>
2023-05-04 06:10:27 +0530

    error page done

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
@@ -7,9 +8,10 @@ const Error = () => {
              <Container fluid className="d-flex flex-column justify-content-center align-items-center" style={{ height: '100vh' }}>
       <Row>
         <Col>
+        <h1 className='text-center'>404</h1>
           <h1>Oops! Something went wrong.</h1>
-          <p>{error.message}</p>
-          <Button variant="primary" onClick={() => window.location.reload()}>Reload</Button>
+          <h1 className='text-center'><Link to="./" ><Button variant="primary" >Home</Button></Link></h1>
+          
         </Col>
       </Row>
     </Container>
